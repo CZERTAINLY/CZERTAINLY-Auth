1062a8e185006db3a395736649f8a958baf9b9e1
lubomirw <76479559+lubomirw@users.noreply.github.com>
2023-07-17 18:48:00 +0200

    Add email to role (#65)

---
+++ src/Czertainly.Auth/Data/Migrations/20230717152116_AddRoleEmail.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Czertainly.Auth.Data.Migrations
+{
+    public partial class AddRoleEmail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "email",
+                schema: "auth",
+                table: "role",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "email",
+                schema: "auth",
+                table: "role");
+        }
+    }
+}
